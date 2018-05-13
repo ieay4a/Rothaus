fefd92f09f462ebfd16eda6b593b4f0764524908
KIM Jinhyeon <ieay4a@kaist.ac.kr>
2018-05-14 01:23:31 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,60 +2,68 @@ var products = [];
 products[0] = {
 name:"Paldo Bibim-myeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/69/320/000043869_0.jpg",
-price: 3,400 Won,
-ingredients: ["Wheat flour","palm oil (Malaysian)","potato starch","vegetable powder","powdered oil S","cotton added alkali (potassium carbonate, sodium carbonate, sodium chloride"]
+price: 3400,
+ingredients: ["Wheat flour","palm oil (Malaysian)","potato starch","vegetable powder","powdered oil S","cotton added alkali (potassium carbonate, sodium carbonate, sodium chloride"],
+beef, diary,
 };
 products[1] = {
 name:"Domestic Black bean Sun-shik(Bong)",
 img:"http://image.homeplus.co.kr/UserFiles/Goods/320/127407858_0.jpg",
-price: 9,990 Won,
-ingredients: ["Brown rice (domestic)","barley (domestic)","rice barley (domestic)","black rice (domestic)","corn (domestic)"]
+price: 9990,
+ingredients: ["Brown rice (domestic)","barley (domestic)","rice barley (domestic)","black rice (domestic)","corn (domestic)"],
+
 };
 products[2] = {
 name:"Nongshim Anseongtang",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/82/320/120074582_0.jpg",
-price: 2,980 WON,
-ingredients: ["Cotton: wheat flour (Australia)","potato starch (German)","palm oil (Malaysian)","rice flour","gluten","carbonic calcium","refined salt","cotton added alkali (Sando)"]
+price: 2980,
+ingredients: ["Cotton: wheat flour (Australia)","potato starch (German)","palm oil (Malaysian)","rice flour","gluten","carbonic calcium","refined salt","cotton added alkali (Sando)"],
+diary, pork, egg, chicken, beef,
 };
 products[3] = {
 name:"Tangjeong Cheongjeongwon rice mushroom soup",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/76/320/000043176_0.jpg",
-price: 1,000 Won,
-ingredients: ["Rice barley rice flour (domestic)","corn flour (imported corn)","butter / milk","starch (imported corn)","oil powder (edible powder","maltdex powder)"]
+price: 1000,
+ingredients: ["Rice barley rice flour (domestic)","corn flour (imported corn)","butter / milk","starch (imported corn)","oil powder (edible powder","maltdex powder)"],
+diary, beef,
 };
 products[4] = {
 name:"Ottogi",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/68/320/102008468_0.jpg",
-price: 1,000 Won,
+price: 1000,
 ingredients: ["Rice (domestic)","refined salt"]
 };
 products[5] = {
 name:"A floating wild whelk",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/42/320/000049042_0.jpg",
-price: 8,990,
-ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt", l"iquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"]
+price: 8990,
+ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt", l"iquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"],
+seafood, 
 };
 products[6] = {
 name:"Nongshim   Nest naengmyeon bibim naengmyeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/62/320/107229462_0.jpg",
-price: 2,980 Won,
-ingredients: ["cotton and wheat flour (U.S.)","potato starch (German)","buckwheat powder mix buckwheat flour (Chinese)","vegetable cream (Dutch)"]
+price: 2980,
+ingredients: ["cotton and wheat flour (U.S.)","potato starch (German)","buckwheat powder mix buckwheat flour (Chinese)","vegetable cream (Dutch)"],
+chicken, beef,
 };
 products[7] = {
 name:"Cheongsoo water cold noodles",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/79/320/123421979_0.jpg",
-price: 3,490 Won,
-ingredients: ["Flour (wheat: Australian, American)","premix buckwheat number 1 (buckwheat flour, wheat flour, wheat gum, gua gum) (Chinese)","sweet potato starch (Chinese, Japanese)","refined cotton salt, and bitter"]
+price: 3490,
+ingredients: ["Flour (wheat: Australian, American)","premix buckwheat number 1 (buckwheat flour, wheat flour, wheat gum, gua gum) (Chinese)","sweet potato starch (Chinese, Japanese)","refined cotton salt, and bitter"],
+beef,
 };
 products[8] = {
 name:"Chicky chewy and refreshing",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/30/320/125369030_0.jpg",
-price: 5,900,
+price: 5900,
 ingredients: ["Chringy noodles","wheat flour (wheat : U.S. rice, Australian)","tapioca starch (Thai)","refined salt","powdered alkalin (cotton added alkali)","wheat flour : liquid fructose","red pepper"]
 };
 products[9] = {
 name:"Ottomer Gongmyeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/64/320/103420964_0.jpg",
-price: 5,990 Won,
-ingredients: ["Fine flour (wheat / Australian, U.S.)","potato starch preparation (potato starch / Chinese, denaturation tapioca / Thai)","seasoned non-friendly: purified water","starch syrup"]
+price: 5990,
+ingredients: ["Fine flour (wheat / Australian, U.S.)","potato starch preparation (potato starch / Chinese, denaturation tapioca / Thai)","seasoned non-friendly: purified water","starch syrup"],
+beef, diary,
 };
