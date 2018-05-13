0162c21d54edead90ef6cf62a84939f2a1ed77b0
matze399 <38873054+matze399@users.noreply.github.com>
2018-05-14 02:04:17 +0800

    Update data.js

--- data.js
+++ data.js
@@ -5,6 +5,12 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/69/320/000043869_0.jpg",
 price: 3400,
 ingredients: ["Wheat flour","palm oil (Malaysian)","potato starch","vegetable powder","powdered oil S","cotton added alkali (potassium carbonate, sodium carbonate, sodium chloride"],
 contains: ["beef", "diary"],
+comments: [
+  { username: "Anton",
+    content: "I really liked the flavor of this product!",
+    reply: [ {username: "Herbert", content: "I agree with you, had the same experience with this product!", 
+	reply: [ {username: "Sarah", content: "I would also add that the price is very good for this portion!"} ] } ]
+}
 };
 products[1] = {
 name:"Domestic Black bean Sun-shik(Bong)",
@@ -12,6 +18,11 @@ img:"http://image.homeplus.co.kr/UserFiles/Goods/320/127407858_0.jpg",
 price: 9990,
 ingredients: ["Brown rice (domestic)","barley (domestic)","rice barley (domestic)","black rice (domestic)","corn (domestic)"],
 contains: [],
+  comments: [
+  { username: "Kevin",
+    content: "This is a bigger portion, you can eat it with your friends.",
+    reply: [ {username: "Sandra", content: "Thanks to your comment, I shared it with my friends. Great!" } ]
+}
 };
 products[2] = {
 name:"Nongshim Anseongtang",
@@ -19,6 +30,11 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/82/320/120074582_0.jpg",
 price: 2980,
 ingredients: ["Cotton: wheat flour (Australia)","potato starch (German)","palm oil (Malaysian)","rice flour","gluten","carbonic calcium","refined salt","cotton added alkali (Sando)"],
 contains: ["diary", "pork", "egg", "chicken", "beef"],
+  comments: [
+  { username: "Samuel",
+    content: "I did not know, that the ingredients are from different countries.. nice information.",
+    reply: [ {username: "Andrea", content: "Now that you mention it... true!"} ]
+}
 };
 products[3] = {
 name:"Tangjeong Cheongjeongwon rice mushroom soup",
@@ -26,6 +42,12 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/76/320/000043176_0.jpg",
 price: 1000,
 ingredients: ["Rice barley rice flour (domestic)","corn flour (imported corn)","butter / milk","starch (imported corn)","oil powder (edible powder","maltdex powder)"],
 contains: ["diary", "beef"],
+  comments: [
+  { username: "Charles",
+    content: "Cheaaaaaaaap!!!!",
+    reply: [ {username: "Tim", content: "Yeah, that's how I like it.", 
+	reply: [ {username: "Caroline", content: "For this price I really expected nothing, but yeah... its good!"} ] } ]
+}
 };
 products[4] = {
 name:"Ottogi",
@@ -33,13 +55,22 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/68/320/102008468_0.jpg",
 price: 1000,
 ingredients: ["Rice (domestic)","refined salt"],
 contains: [],
+  comments: [
+  { username: "Richard",
+    content: "Some good rice."}
 };
 products[5] = {
 name:"A floating wild whelk",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/42/320/000049042_0.jpg",
 price: 8990,
-ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt", l"iquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"],
+ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt", "liquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"],
 contains: ["seafood"],
+comments: [
+  { username: "Donald",
+    content: "First thing that I bought from Homeplus, pretty nice.",
+    reply: [ {username: "Ahmed", content: "For me it was okay, but nothing special.", 
+	reply: [ {username: "Cristiano", content: "I'll give it a try tomorrow."} ] } ]
+}
 };
 products[6] = {
 name:"Nongshim   Nest naengmyeon bibim naengmyeon",
@@ -47,6 +78,12 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/62/320/107229462_0.jpg",
 price: 2980,
 ingredients: ["cotton and wheat flour (U.S.)","potato starch (German)","buckwheat powder mix buckwheat flour (Chinese)","vegetable cream (Dutch)"],
 contains: ["chicken", "beef"],
+comments: [
+  { username: "Tamara",
+    content: "Some international ingredients! Seeing my country in korea. Haha!",
+    reply: [ {username: "Lisa", content: "Don't forget about the taste. I liked it.", 
+	reply: [ {username: "Cassandra", content: "I agree with Lisa."} ] } ]
+}
 };
 products[7] = {
 name:"Cheongsoo water cold noodles",
@@ -54,6 +91,11 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/79/320/123421979_0.jpg",
 price: 3490,
 ingredients: ["Flour (wheat: Australian, American)","premix buckwheat number 1 (buckwheat flour, wheat flour, wheat gum, gua gum) (Chinese)","sweet potato starch (Chinese, Japanese)","refined cotton salt, and bitter"],
 contains: ["beef"],
+  comments: [
+  { username: "Raphael",
+    content: "I was first sceptical because of the word cold in front of noodles, but... it was acceptable!",
+    reply: [ {username: "Lara", content: "For this price, very suitable!" } ]
+}
 };
 products[8] = {
 name:"Chicky chewy and refreshing",
@@ -61,6 +103,12 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/30/320/125369030_0.jpg",
 price: 5900,
 ingredients: ["Chringy noodles","wheat flour (wheat : U.S. rice, Australian)","tapioca starch (Thai)","refined salt","powdered alkalin (cotton added alkali)","wheat flour : liquid fructose","red pepper"],
 contains: [],
+comments: [
+  { username: "Michael",
+    content: "As the name is saying... it was refreshing!!",
+    reply: [ {username: "Ryan", content: "Lol, for me not really refreshing.. but was still tasty.", 
+	reply: [ {username: "Andrine", content: "I would recommend!"} ] } ]
+}
 };
 products[9] = {
 name:"Ottomer Gongmyeon",
@@ -68,4 +116,10 @@ img:"http://image.homeplus.co.kr/UserFiles/Pressimages/64/320/103420964_0.jpg",
 price: 5990,
 ingredients: ["Fine flour (wheat / Australian, U.S.)","potato starch preparation (potato starch / Chinese, denaturation tapioca / Thai)","seasoned non-friendly: purified water","starch syrup"],
 contains: ["beef", "diary"],
+  comments: [
+  { username: "Tom",
+    content: "Something new for me... not my favorite one, but it's okay.",
+    reply: [ {username: "Hillary", content: "I enjoyed it!", 
+	reply: [ {username: "Malcolm", content: "My friends did not like this product, but for me it was quiet tasty. I think it differs on person!"} ] } ]
+}
 };
