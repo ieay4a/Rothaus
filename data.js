3f7eef4f485cf6e91bf9910355fa0121c33d17dd
Jinhyeon <ieay4a@kaist.ac.kr>
2018-05-14 02:50:10 +0900

    Revert "Data_Update_Comment"
    This reverts commit 9339fcbadc98631ac220cf79d5a631ba3c6c51f7.

--- data.js
+++ data.js
@@ -2,114 +2,70 @@ var products = [];
 products[0] = {
 name:"Paldo Bibim-myeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/69/320/000043869_0.jpg",
-price: 3,400 Won,
-ingredients: ["Wheat flour","palm oil (Malaysian)","potato starch","vegetable powder","powdered oil S","cotton added alkali (potassium carbonate, sodium carbonate, sodium chloride"]
-comments: [
-  { username: "Anton",
-    content: "I really liked the flavor of this product!",
-    reply: [ {username: "Herbert", content: "I agree with you, had the same experience with this product!", 
-	reply: [ {username: "Sarah", content: "I would also add that the price is very good for this portion!"} ] } ]
-}
+price: 3400,
+ingredients: ["Wheat flour","palm oil (Malaysian)","potato starch","vegetable powder","powdered oil S","cotton added alkali (potassium carbonate, sodium carbonate, sodium chloride"],
+contains: ["beef", "diary"],
 };
 products[1] = {
 name:"Domestic Black bean Sun-shik(Bong)",
 img:"http://image.homeplus.co.kr/UserFiles/Goods/320/127407858_0.jpg",
-price: 9,990 Won,
-ingredients: ["Brown rice (domestic)","barley (domestic)","rice barley (domestic)","black rice (domestic)","corn (domestic)"]
-comments: [
-  { username: "Kevin",
-    content: "This is a bigger portion, you can eat it with your friends.",
-    reply: [ {username: "Sandra", content: "Thanks to your comment, I shared it with my friends. Great!" } ]
-}
+price: 9990,
+ingredients: ["Brown rice (domestic)","barley (domestic)","rice barley (domestic)","black rice (domestic)","corn (domestic)"],
+contains: [],
 };
 products[2] = {
 name:"Nongshim Anseongtang",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/82/320/120074582_0.jpg",
-price: 2,980 WON,
-ingredients: ["Cotton: wheat flour (Australia)","potato starch (German)","palm oil (Malaysian)","rice flour","gluten","carbonic calcium","refined salt","cotton added alkali (Sando)"]
-comments: [
-  { username: "Samuel",
-    content: "I did not know, that the ingredients are from different countries.. nice information.",
-    reply: [ {username: "Andrea", content: "Now that you mention it... true!"} ]
-}
+price: 2980,
+ingredients: ["Cotton: wheat flour (Australia)","potato starch (German)","palm oil (Malaysian)","rice flour","gluten","carbonic calcium","refined salt","cotton added alkali (Sando)"],
+contains: ["diary", "pork", "egg", "chicken", "beef"],
 };
 products[3] = {
 name:"Tangjeong Cheongjeongwon rice mushroom soup",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/76/320/000043176_0.jpg",
-price: 1,000 Won,
-ingredients: ["Rice barley rice flour (domestic)","corn flour (imported corn)","butter / milk","starch (imported corn)","oil powder (edible powder","maltdex powder)"]
-comments: [
-  { username: "Charles",
-    content: "Cheaaaaaaaap!!!!",
-    reply: [ {username: "Tim", content: "Yeah, that's how I like it.", 
-	reply: [ {username: "Caroline", content: "For this price I really expected nothing, but yeah... its good!"} ] } ]
-}
+price: 1000,
+ingredients: ["Rice barley rice flour (domestic)","corn flour (imported corn)","butter / milk","starch (imported corn)","oil powder (edible powder","maltdex powder)"],
+contains: ["diary", "beef"],
 };
 products[4] = {
 name:"Ottogi",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/68/320/102008468_0.jpg",
-price: 1,000 Won,
-ingredients: ["Rice (domestic)","refined salt"]
-comments: [
-  { username: "Richard",
-    content: "Some good rice."}
+price: 1000,
+ingredients: ["Rice (domestic)","refined salt"],
+contains: [],
 };
 products[5] = {
 name:"A floating wild whelk",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/42/320/000049042_0.jpg",
-price: 8,990,
-ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt","liquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"]
-comments: [
-  { username: "Donald",
-    content: "First thing that I bought from Homeplus, pretty nice.",
-    reply: [ {username: "Ahmed", content: "For me it was okay, but nothing special.", 
-	reply: [ {username: "Cristiano", content: "I'll give it a try tomorrow."} ] } ]
-}
+price: 8990,
+ingredients: ["Whelk (imported)","soy sauce (imported)","wheat (imported)","refined salt", l"iquid fructose, alcohol)","sugar","liquid fructose","glycine","pytin","purified water"],
+contains: ["seafood"],
 };
 products[6] = {
-name:"Nongshim Nest naengmyeon bibim naengmyeon",
+name:"Nongshim   Nest naengmyeon bibim naengmyeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/62/320/107229462_0.jpg",
-price: 2,980 Won,
-ingredients: ["cotton and wheat flour (U.S.)","potato starch (German)","buckwheat powder mix buckwheat flour (Chinese)","vegetable cream (Dutch)"]
-comments: [
-  { username: "Tamara",
-    content: "Some international ingredients! Seeing my country in korea. Haha!",
-    reply: [ {username: "Lisa", content: "Don't forget about the taste. I liked it.", 
-	reply: [ {username: "Cassandra", content: "I agree with Lisa."} ] } ]
-}
+price: 2980,
+ingredients: ["cotton and wheat flour (U.S.)","potato starch (German)","buckwheat powder mix buckwheat flour (Chinese)","vegetable cream (Dutch)"],
+contains: ["chicken", "beef"],
 };
 products[7] = {
 name:"Cheongsoo water cold noodles",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/79/320/123421979_0.jpg",
-price: 3,490 Won,
-ingredients: ["Flour (wheat: Australian, American)","premix buckwheat number 1 (buckwheat flour, wheat flour, wheat gum, gua gum) (Chinese)","sweet potato starch (Chinese, Japanese)","refined cotton salt, and bitter"]
-comments: [
-  { username: "Raphael",
-    content: "I was first sceptical because of the word cold in front of noodles, but... it was acceptable!",
-    reply: [ {username: "Lara", content: "For this price, very suitable!" } ]
-}
+price: 3490,
+ingredients: ["Flour (wheat: Australian, American)","premix buckwheat number 1 (buckwheat flour, wheat flour, wheat gum, gua gum) (Chinese)","sweet potato starch (Chinese, Japanese)","refined cotton salt, and bitter"],
+contains: ["beef"],
 };
 products[8] = {
 name:"Chicky chewy and refreshing",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/30/320/125369030_0.jpg",
-price: 5,900,
-ingredients: ["Chringy noodles","wheat flour (wheat : U.S. rice, Australian)","tapioca starch (Thai)","refined salt","powdered alkalin (cotton added alkali)","wheat flour : liquid fructose","red pepper"]
-comments: [
-  { username: "Michael",
-    content: "As the name is saying... it was refreshing!!",
-    reply: [ {username: "Ryan", content: "Lol, for me not really refreshing.. but was still tasty.", 
-	reply: [ {username: "Andrine", content: "I would recommend!"} ] } ]
-}
+price: 5900,
+ingredients: ["Chringy noodles","wheat flour (wheat : U.S. rice, Australian)","tapioca starch (Thai)","refined salt","powdered alkalin (cotton added alkali)","wheat flour : liquid fructose","red pepper"],
+contains: [],
 };
 products[9] = {
 name:"Ottomer Gongmyeon",
 img:"http://image.homeplus.co.kr/UserFiles/Pressimages/64/320/103420964_0.jpg",
-price: 5,990 Won,
-ingredients: ["Fine flour (wheat / Australian, U.S.)","potato starch preparation (potato starch / Chinese, denaturation tapioca / Thai)","seasoned non-friendly: purified water","starch syrup"]
-comments: [
-  { username: "Tom",
-    content: "Something new for me... not my favorite one, but it's okay.",
-    reply: [ {username: "Hillary", content: "I enjoyed it!", 
-	reply: [ {username: "Malcolm", content: "My friends did not like this product, but for me it was quiet tasty. I think it differs on person!"} ] } ]
-}
+price: 5990,
+ingredients: ["Fine flour (wheat / Australian, U.S.)","potato starch preparation (potato starch / Chinese, denaturation tapioca / Thai)","seasoned non-friendly: purified water","starch syrup"],
+contains: ["beef", "diary"],
 };
