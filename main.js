44335a09531aa6757536c2c0c752129419c9f945
tykimseoul <tykimseoul@gmail.com>
2018-05-14 16:27:41 +0900

    filter design

--- main.js
+++ main.js
@@ -11,23 +11,6 @@ $(document).ready(function () {
     sortTable = document.getElementById('sortTable');
     excluding = [0, 0, 0, 0];
     sortMethod = 0;
-
-    barcodePanel.hide();
-    $('#barcodeButton').click(function () {
-        filterPanel.hide();
-        barcodePanel.toggle();
-    });
-
-    var filterPanel = $('#filterPanel');
-    filterPanel.hide();
-    $('#filterButton').click(function () {
-        barcodePanel.hide();
-        filterPanel.toggle();
-    });
-    
-
-
-
     x = document.getElementsByClassName("customSelect");
     for (i = 0; i < x.length; i++) {
         selElmnt = x[i].getElementsByTagName("select")[0];
@@ -77,27 +60,95 @@ $(document).ready(function () {
         });
     }
     document.addEventListener("click", closeAllSelect);
-    
+    barcodePanel.hide();
+    $('#barcodeButton').click(function () {
+        filterPanel.hide();
+        barcodePanel.toggle();
+    });
+
+    var filterPanel = $('#filterPanel');
+    filterPanel.hide();
+    $('#filterButton').click(function () {
+        barcodePanel.hide();
+        filterPanel.toggle();
+    });
+
+
+    var searchinput = $('#searchinput');
+    var category = $('#category');
+    var exclude = $('#filter').prop('exclude');
+    var sortby = $('#filter').prop('sort');
+
+    category.selectmenu();
+    $('input[type=checkbox]').checkboxradio();
+    $('input[type=radio]').checkboxradio();
+    $('#filter input').change(updateSearch);
+    $('#searchButton').click(updateSearch);
+    searchinput.change(updateSearch);
+
+
+    var resultlist = $('#resultlist');
+    var trendinglist = $('#trendinglist');
+    var newlist = $('#newlist');
+    resultlist.parent().hide();
+
+    //How to revert to suggenstions??
+    function updateSearch() {
+        resultlist.parent().show();
+        trendinglist.parent().hide();
+        newlist.parent().hide();
+        resultlist.empty();
+
+        results = Object.keys(products).filter(pid =>
+            !products[pid].contains.some(ing => $("#" + ing).prop("checked"))
+        );
+        resultlist.append(results.map(pid_to_product));
+    }
+
+    trendings = [1, 3, 5];
+    news = [2, 4, 6];
+    trendinglist.append(
+        trendings.map(pid_to_product)
+    )
+    newlist.append(
+        news.map(pid_to_product)
+    )
+
+    function pid_to_product(pid) {
+        var p = products[pid];
+        return $('<li>').append(
+            $('<a href="product.html?productid=' + pid + '">').append(
+                $('<img>').attr('src', p.img),
+                $('<div class="productname">').text(p.name),
+                $('<span class="contains">').append(p.contains),
+                $('<span class="price">').text(p.price + " KRW")
+            ));
+    }
+
+
+
+
+
 });
 
 function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
+    /*a function that will close all select boxes in the document,
+    except the current select box:*/
+    var x, y, i, arrNo = [];
+    x = document.getElementsByClassName("select-items");
+    y = document.getElementsByClassName("select-selected");
+    for (i = 0; i < y.length; i++) {
+        if (elmnt == y[i]) {
+            arrNo.push(i)
+        } else {
+            y[i].classList.remove("select-arrow-active");
+        }
     }
-  }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
+    for (i = 0; i < x.length; i++) {
+        if (arrNo.indexOf(i)) {
+            x[i].classList.add("select-hide");
+        }
     }
-  }
 }
 
 function toggleExcluding(idx) {
